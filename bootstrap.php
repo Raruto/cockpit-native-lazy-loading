a7c82f7f611314e36da9889d6fdcf8586731ec56
Raruto <Raruto@users.noreply.github.com>
2021-01-29 01:08:17 +0100

    lift up

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Cockpit native lazy loading addon
+ *
+ * @see https://web.dev/browser-level-image-lazy-loading/
+ *
+ * @author  Raruto
+ * @package cockpit-blog
+ * @license MIT
+ */
+
+/**
+ * Automatically add img[loading="lazy"] attribute ("html" and "wysiwyg" editor)
+ */
+if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
+  $this->on('admin.init', function() {
+    // load custom js
+    $this->helper('admin')->addAssets(['nativelazyloading:assets/editor.js']);
+  });
+}
